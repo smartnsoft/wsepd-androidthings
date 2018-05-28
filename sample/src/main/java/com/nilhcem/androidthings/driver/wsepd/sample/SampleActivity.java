2b0ef66c99c3b4b765f13d99ad0428b0f7da9d58
David Fournier <david.fournier@smartnsoft.com>
2018-05-28 15:59:30 +0200

    merge

--- sample/src/main/java/com/nilhcem/androidthings/driver/wsepd/sample/SampleActivity.java
+++ sample/src/main/java/com/nilhcem/androidthings/driver/wsepd/sample/SampleActivity.java
@@ -2,16 +2,27 @@ package com.nilhcem.androidthings.driver.wsepd.sample;
 
 import java.io.IOException;
 
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.MeasureSpec;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.nilhcem.androidthings.driver.wsepd.DeviceType;
 import com.nilhcem.androidthings.driver.wsepd.EPaperDisplay;
 import com.nilhcem.androidthings.driver.wsepd.EPaperDisplayFactory;
+
+import java.io.IOException;
 
 public class SampleActivity
     extends Activity
@@ -44,10 +55,9 @@ public class SampleActivity
 
   private EPaperDisplay display;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState)
-  {
-    super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
     try
     {
@@ -69,10 +79,21 @@ public class SampleActivity
 
       Log.d(TAG, "Refreshing");
       // Draw a black-on-white bitmap
-      final Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.rocket);
-      display.setPixels(bmp);
-      display.refresh();
-      Thread.sleep(1000);
+
+      final View root = getLayoutInflater().inflate(R.layout.test, null, false);
+      ((ViewGroup) root.findViewById(R.id.informations)).addView(new InformationLayout(getApplicationContext(), "Qualité", 3.65f));
+      ((ViewGroup) root.findViewById(R.id.informations)).addView(new InformationLayout(getApplicationContext(), "Prix", 2.15f));
+      try
+      {
+        display = EPaperDisplayFactory.create(screenPinout.spiName, screenPinout.busyGPIO, screenPinout.resetGPIO, screenPinout.dcGPIO, DeviceType.Preset.EPD7X5B.deviceType);
+        display.setPixels(root);
+        display.refresh();
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+      }
+
       Log.d(TAG, "Refreshed !");
 
     }
@@ -96,4 +117,5 @@ public class SampleActivity
       Log.e(TAG, "Error closing display", e);
     }
   }
+
 }
